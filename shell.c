095c92184b93ecf487ab2e4137a404ba9fe35287
Jason Lin <jasonlin43212@gmail.com>
2018-11-29 06:05:40 -0500

    found the special redirection symbols

--- shell.c
+++ shell.c
@@ -52,29 +52,66 @@ char * read_input(){
   }
 }
 
+
+int sep_special(char ** old, char *** output, char * special, int * arg_index){
+  if (strstr(*old,special)){
+    char * super_old = *old;
+    strsep(&(*old),special);
+    if (*super_old){
+      (*output)[*arg_index] = super_old;
+      (*arg_index)++;
+    }
+    (*output)[*arg_index] = special;
+    (*arg_index)++;
+    printf("special: %s, %s\n",special,*old);
+    *old += strlen(special)-1;
+    if (**old){
+      (*output)[*arg_index] = *old;
+      (*arg_index)++;
+      *old += strlen(*old);
+    }
+    return 1;
+  }
+  return 0;
+}
+
 char ** parse_args(char * line){
   //Remove whitespace in front of input
   while(*line == ' ' || *line == '\t'){
     line++;
   }
   char ** output = malloc(10*sizeof(char *));
-  int num_args = 0;
+  int arg_index = 0;
   char * old = NULL;
 
   while(line){
     old = line;
     strsep(&line," ");
-    if (*old != '\0'){
-      output[num_args] = old;
-      num_args++;
+    int num_special = 0;
+    //Checks for redirects
+    num_special += sep_special(&old,&output,">>",&arg_index);
+    num_special += sep_special(&old,&output,"<<",&arg_index);
+    num_special += sep_special(&old,&output,">",&arg_index);
+    num_special += sep_special(&old,&output,"<",&arg_index);
+    num_special += sep_special(&old,&output,"|",&arg_index);
+    if (num_special < 1 && *old){
+      output[arg_index] = old;
+      arg_index++;
     }
   }
-  output[num_args] = NULL;
+  output[arg_index] = NULL;
 
   return output;
 }
 
 int execute_args(char ** args){
+  /*if (has special symbol while creating a 2d array of both the array and the symbol by giving it inside,){
+  then pass the 2d array into a special function that handles it case by case
+  OR
+  use switch statement to look at the 2nd element and based on that
+  execute different functions
+}
+*/
   int child_pid = fork();
   if (child_pid == -1){
     printf("Failed to fork child.\n");
@@ -103,9 +140,7 @@ void change_directory_display(char output[]){
     exit(1);
   }
 
-  char * username = getpwuid(getuid())->pw_name;
-  char home[4096] = "/home/";
-  strcat(home,username);
+  char * home = getenv("HOME");
   if (strstr(path,home)){
     strcpy(output,"~");
     strcat(output,path+strlen(home));
@@ -134,13 +169,13 @@ int main(){
     while (cur_input){
       //printf("Your input now is cur:%s or input:%s\n",cur_input,input);
       args = parse_args(cur_input);
-      //print_list(args);
+      print_list(args);
+
       //Checks if the user is trying to change directory
       if (strcmp(args[0],"cd") == 0){
         //User has no args or "~"
         if (args[1] == NULL || strcmp(args[1],"~") == 0){
-          char home[4096] = "/home/";
-          strcat(home,username);
+          char * home = getenv("HOME");
           chdir(home);
         }
         //User puts in one arg
@@ -156,9 +191,13 @@ int main(){
         //Change directory that is displayed
         change_directory_display(current_directory);
       }
+
+      //Exit
       else if (strcmp(args[0],"exit") == 0){
         exit(0);
       }
+
+      //All Other Commands
       else{
         status = execute_args(args);
       }
