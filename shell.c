4175be9e04ce2a3d6d4a646c07f67b3c4d7dd6ba
Jason Lin <jasonlin43212@gmail.com>
2018-12-12 15:00:04 -0500

    readded some shell

--- shell.c
+++ shell.c
@@ -111,6 +111,7 @@ int main(){
 
   while(status){
     //printf("%s@%s:%s$ ",username,host_name,current_directory);
+    printf("$ ");
     input = read_input();
     char * cur_input = input;
     strsep(&input,";");
